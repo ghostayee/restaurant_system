ec32409637caf15ea654c733da4d47b3c22d1432
ghostayee <mayeeayee@outlook.com>
2026-05-15 14:31:51 +0300

    user login ad interface

--- app/__init__.py
+++ app/__init__.py
@@ -4,31 +4,24 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
 
-# Extensions
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 socketio = SocketIO()
 
 login_manager.login_view = "auth.login"
-login_manager.login_message_category = "info"
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
 
-    # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
     socketio.init_app(app)
 
-    # Register Models
-    with app.app_context():
-        import app.models.user
-
-    # Register Blueprints
+    # Blueprints - Import inside function to avoid circular import
     from app.routes.auth_routes import auth
     from app.routes.main_routes import main
 
@@ -38,7 +31,6 @@ def create_app():
     return app
 
 
-# User Loader
 @login_manager.user_loader
 def load_user(user_id):
     from app.models.user import User
